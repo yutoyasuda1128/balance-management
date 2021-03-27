0a8e87a662d3a4352607467c6f7b72a483db8c05
yutoyasuda1128 <yuuto1206a@gmail.com>
2021-03-27 05:58:57 +0000

    モーダルウインドウを表示した

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { ModalWindow } from './ModalWindow';
 
 export function AppCalendar() {
     const [dateObj , setDateObj] = useState(new Date());
@@ -11,6 +12,12 @@ export function AppCalendar() {
             onChange={setDateObj}
             value={dateObj}
           />
+          <div>
+            <button>Click</button>
+          </div>
+          <div id="overlay">
+            <ModalWindow />
+          </div>
         </div>
     );
 }
