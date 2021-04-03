cb705907700a02db084526ff146d6e89764402b5
yutoyasuda1128 <yuuto1206a@gmail.com>
2021-04-03 04:59:15 +0000

    モーダルウインドウにテキストの追加

--- src/ModalWindow.jsx
+++ src/ModalWindow.jsx
@@ -5,7 +5,9 @@ export function ModalWindow(props){
   console.log(props.date)
   return (
     <div className="ModalWindow-Content">
-      <p>これがモーダルウィンドウです。</p>
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
       <p><button onClick={props.onClose}>close</button></p>
     </div>
   )
