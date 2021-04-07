6ed87dd8ead9aeea3855137fe77af4c421a6b8bd
yutoyasuda1128 <yuuto1206a@gmail.com>
2021-04-07 11:35:16 +0000

    モーダル内に支出収入ボタンの追加

--- src/ModalWindow.jsx
+++ src/ModalWindow.jsx
@@ -1,13 +1,32 @@
 import React, { useState } from 'react';
 import './ModalWindow.css';
 
+
+function formatDate(dt) {
+  const y = dt.getFullYear();
+  const m = ('00' + (dt.getMonth()+1)).slice(-2);
+  const d = ('00' + dt.getDate()).slice(-2);
+  return (y + '-' + m + '-' + d);
+}
 export function ModalWindow(props){
   console.log(props.date)
+  console.log(props.date.getFullYear())
   return (
     <div className="ModalWindow-Content">
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
+      <div>{ formatDate(props.date)}
+      　<div className="Expenditureincome">
+          <button>支出</button>
+          <button>収入</button>
+        </div>
+      </div>
+      <div className="ModalWindow-InputArea">
+        <select>
+          <option value="">カテゴリ</option>
+          <option value="food">食費</option>
+          <option value="miscellaneous">雑費</option>
+        </select>
+        <input type="number"  placeholder="金額"/>
+      </div>
       <p><button onClick={props.onClose}>close</button></p>
     </div>
   )
