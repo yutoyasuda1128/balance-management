d6006680902d4751f6896f6236b6fa8686c272e5
yutoyasuda1128 <yuuto1206a@gmail.com>
2021-04-16 12:09:46 +0000

    収支の合計額のボタンの設置

--- src/ModalWindow.jsx
+++ src/ModalWindow.jsx
@@ -9,25 +9,50 @@ function formatDate(dt) {
   return (y + '-' + m + '-' + d);
 }
 export function ModalWindow(props){
-  console.log(props.date)
-  console.log(props.date.getFullYear())
+  const [price, setPrice] = useState(0);
+  const handleInput = (e) => {
+    setPrice(e.target.value)
+  }
+  
+  const [category, setCategory] = useState(""); 
+  const handleCategory = (e) => {
+    setCategory(e.target.value)
+  }
+  
+  const registerExpense = () => {
+    const obj = {
+      type: "expense", 
+      price,
+      category
+    }
+    
+    window.localStorage.setItem("balance-management", JSON.stringify([obj]))
+  }
+  
   return (
     <div className="ModalWindow-Content">
       <div className="ModalWindow-ButtonArea">{ formatDate(props.date)}
       　<div className="Expenditureincome">
-          <button>支出</button>
+          <button onClick={registerExpense}>支出</button>
           <button>収入</button>
         </div>
       </div>
       <div className="ModalWindow-InputArea">
-        <select className="ModalWindow-CategoryArea">
+        <select className="ModalWindow-CategoryArea" onChange={handleCategory}>
           <option value="">カテゴリ</option>
           <option value="food">食費</option>
           <option value="miscellaneous">雑費</option>
         </select>
-        <input className="ModalWindow-Input" type="number"  placeholder="金額"/>
+        <input className="ModalWindow-Input" type="number"  placeholder="金額" onChange={handleInput}/>
+      </div>
+      <div className="ModalWindow-total-amount">
+        <input className="ModalWindow-total" type="text" value="合計額" disabled/>
+        <input className="ModalWindow-expense" type="text" value="支出" disabled/>
+        <input className="ModalWindow-expense-total" type="number" disabled/>
+        <input className="ModalWindow-income" type="text" value="収入" disabled/>
+        <input className="ModalWindow-income-total" type="number" disabled/>
       </div>
       <p><button onClick={props.onClose}>close</button></p>
     </div>
-  )
+  );
 }
